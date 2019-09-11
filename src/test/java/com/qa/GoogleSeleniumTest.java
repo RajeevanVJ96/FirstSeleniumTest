b0ef652f7e270c1cc156d74d0dd0704e6239d466
RajeevanVJ96 <rvijay46@gmail.com>
2019-09-11 11:06:15 +0100

    Two Inputs test completed

--- src/test/java/com/qa/GoogleSeleniumTest.java
+++ src/test/java/com/qa/GoogleSeleniumTest.java
@@ -30,11 +30,29 @@ public class GoogleSeleniumTest {
     }
 
     @Test
+    public void twoInputTest() throws InterruptedException{
+        driver.manage().window().maximize();
+        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+        Thread.sleep(5000);
+        WebElement inputA = driver.findElement(By.id("sum1"));
+        assertTrue(inputA.isDisplayed());
+        inputA.sendKeys("2");
+        WebElement inputB = driver.findElement(By.id("sum2"));
+        assertTrue(inputB.isDisplayed());
+        inputB.sendKeys("2");
+        Thread.sleep(5000);
+        WebElement submit = driver.findElement(By.xpath("//*[@id=\"gettotal\"]/button"));
+        submit.click();
+        Thread.sleep(5000);
+
+    }
+
+    @Test
     public void searchTest() throws InterruptedException {
         driver.manage().window().maximize();
         driver.get("http://google.com");
         Thread.sleep(2000);
-        WebElement searchField = driver.findElement(By.name("q"));
+        WebElement searchField = driver.findElement(By.id("q"));
         assertTrue(searchField.isDisplayed());
         searchField.sendKeys("funny cat pictures");
         Thread.sleep(5000);
@@ -51,6 +69,8 @@ public class GoogleSeleniumTest {
         Thread.sleep(5000);
         jse.executeScript("scroll(0, -250);");
         Thread.sleep(5000);
+
+        driver.findElementByXPath("/html/body/div[6]/div[3]/div[3]/div[2]/div/div[2]/div[2]/div/div/div/div/div[2]/div[1]/div[7]");
 
     }
 
